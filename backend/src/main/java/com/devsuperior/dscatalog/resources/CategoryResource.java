5cd3e933fa3c39add3b2a9aa4fcd6785ba0b71ac
Erick Oliveira <erickoliveira@rorato.local>
2022-02-03 14:05:15 -0300

    PUT Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -4,9 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,17 @@ public class CategoryResource {
     public ResponseEntity<List<Category>> findAll(){
         List<Category> list = service.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Category findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @PutMapping
+    public Category save(@RequestBody Category category){
+        Category newCategory = service.save(category);
+        return newCategory;
     }
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -14,6 +15,17 @@ public class CategoryService {
 
     public List<Category> findAll(){
         return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Category newCategory = repository.findById(id).get();
+        return newCategory;
+    }
+
+    @Transactional
+    public Category save(Category category){
+        Category newCategory = repository.saveAndFlush(category);
+        return newCategory;
     }
 
 }
