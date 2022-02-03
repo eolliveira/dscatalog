0e7a6bd2ae9e52167768e7f79aaadcf0e002ed6e
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-02-02 23:06:19 -0300

    implementing CategoryReasource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog.entities;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public Product(){}
+
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscatalog.entities;
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
