133be66bc5a939a4b85e7d655b61b8ef5120d0a2
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-02-06 21:38:32 -0300

    ProductDTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,71 +1,23 @@
 package com.devsuperior.dscatalog.services;
-import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
-import com.devsuperior.dscatalog.services.exceptions.ResourcesNotFoundException;
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
-
 @Service
-public class CategoryService {
+public class ProductService {
 
     @Autowired
-    private CategoryRepository repository;
+    private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAll(Pageable pageable){
-        Page<Category> list = repository.findAll(pageable);
-        return list.map(CategoryDTO::new);
-        //return list.stream().map(c -> new CategoryDTO(c)).collect(Collectors.toList());
-    }
-
-    @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id){
-       Optional<Category> opt = repository.findById(id);
-       Category cat = opt.orElseThrow(() -> new ResourcesNotFoundException("entity not found"));
-        return new CategoryDTO(cat);
-    }
-
-    @Transactional
-    public CategoryDTO insert(CategoryDTO dto){
-        Category category = new Category();
-        category.setName(dto.getName());
-        category = repository.save(category);
-        return new CategoryDTO(category);
-    }
-
-    @Transactional
-    public CategoryDTO update(Long id, CategoryDTO dto) {
-        try {
-            Category category = repository.getById(id);
-            category.setName(dto.getName());
-            category = repository.save(category);
-            return new CategoryDTO(category);
-        }
-        catch (EntityNotFoundException e){
-          throw new ResourcesNotFoundException("resource Id:" + id + " not found");
-        }
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        try {
-            Category category = repository.getById(id);
-            repository.delete(category);
-        }
-        catch (EntityNotFoundException e){
-            throw new ResourcesNotFoundException("resource Id:" + id + " not found");
-        }
-        catch (DataIntegrityViolationException e) {
-            throw new DataBaseException("Database integrity violation");
-        }
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> list = repository.findAll(pageable);
+        return list.map(ProductDTO::new);
     }
 }
