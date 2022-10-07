410fa1104c4860d768ce0edb86af31be8482eea0
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-07 11:21:25 -0300

    teste0222

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+
+
 @Configuration
 public class AppConfig {
 
